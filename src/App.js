a691f04ba84c89e6b6af047d529a95052139b887
si-shakil <sishakil850@gmail.com>
2022-05-10 10:17:17 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./Components/Home/HomePage/HomePage";
 import Navbar from "./Components/Shared/Navbar/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+      </Routes>
     </div>
   );
 }

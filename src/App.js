994e886bcc2c3d44b522acc853d22ba3160b1b13
si-shakil <sishakil850@gmail.com>
2022-05-10 17:13:55 +0600

    footer added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import Footer from "./Components/Home/HomePage/Footer/Footer";
 import HomePage from "./Components/Home/HomePage/HomePage";
 import Navbar from "./Components/Shared/Navbar/Navbar";
 
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
       </Routes>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Components/Home/HomePage/Footer/Footer.js
@@ -0,0 +1,36 @@
+import React from "react";
+import footerBackground from "../../../../assets/images/footer.png";
+
+const Footer = () => {
+  return (
+    <footer
+      class="footer px-20 pb-10 flex justify-between text-black"
+      style={{
+        background: `url(${footerBackground})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+      }}
+    >
+      <div>
+        <span class="footer-title">Services</span>
+        <a class="link link-hover">Emergency Checkup</a>
+        <a class="link link-hover">Monthly Checkup</a>
+        <a class="link link-hover">Weekly Checkup</a>
+        <a class="link link-hover">Deep Checkup</a>
+      </div>
+      <div>
+        <span class="footer-title">Oral Health</span>
+        <a class="link link-hover">Fluoride Treatment</a>
+        <a class="link link-hover">Cavity Filling</a>
+        <a class="link link-hover">Teeth Whitening</a>
+      </div>
+      <div>
+        <span class="footer-title">Our Address</span>
+        <a class="link link-hover">New York - 10101010 Hudson</a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
